b7fa18d112cf5993f2fc4bd6c0e8d249826a4199
⚝αикιт кυмαя⚝ <98248106+xNKIT@users.noreply.github.com>
2022-02-16 16:37:51 +0530

    Create practiceifelse.java

---
+++ practiceifelse.java
@@ -0,0 +1,14 @@
+/*class practiceifelse {
+    public static void main(String[] args) {
+        
+    int a = 10;
+    if (a=11) {
+            System.out.println("I am 14");
+    }
+    else{
+            System.out.println("I am not 14");
+    
+        }
+    }
+}
+*/
