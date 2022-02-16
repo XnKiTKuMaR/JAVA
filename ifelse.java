9cc8b771edad1a2df74c39d86d28a219208a28d4
⚝αикιт кυмαя⚝ <98248106+xNKIT@users.noreply.github.com>
2022-02-16 16:35:59 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class ifelse
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a>=18) {
+	        System.out.println("You can drive");
+        }
+         else {
+            System.out.println("You Cannot Drive");
+        }
+    }
+}
